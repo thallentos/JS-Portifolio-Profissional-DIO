424ed1589c4469d66839fdbed9948b251d5a1e96
Thallentos <thallescmarques@gmail.com>
2025-05-11 05:22:35 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+(async () => {
+    const profileData = await fetchProfileData();
+    console.log(profileData);
+    // renderHeader(profileData);
+    // renderLanguages(profileData);
+    // renderPortfolio(profileData);
+    // renderExperience(profileData);
+    // renderSkills(profileData);
+})()
