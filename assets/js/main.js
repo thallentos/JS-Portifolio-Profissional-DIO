d242d8f6221c4573b1693d5cd8bab51eb9fa46c2
Thallentos <thallescmarques@gmail.com>
2025-05-14 17:51:10 -0300

    alterações nas softskills através do json

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,15 @@ function updateProfileInfo(profileData) {
   email.href = `mailto:${profileData.email}`;
 }
 
+function updateSoftSkills(profileData) {
+  const softSkills = document.getElementById("profile.skills.softSkills");
+  softSkills.innerHTML = profileData.skills.softSkills
+    .map((skill) => "<li>${skill}</li>")
+    .join("");
+}
+
 (async () => {
   const profileData = await fetchProfileData();
   updateProfileInfo(profileData);
+  updateSoftSkills(profileData);
 })();
