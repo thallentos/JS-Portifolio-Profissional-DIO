d85a8952e4b668019c84107f7ccbb4b0d67858d7
Thallentos <thallescmarques@gmail.com>
2025-05-14 20:37:21 -0300

    correção do erro lógico nas hardSkills

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ function updateSoftSkills(profileData) {
 
 function updateHardSkills(profileData) {
   const hardSkills = document.getElementById("profile.skills.hardSkills");
-  hardSkills.innerHTML = profile.skills.hardSkills
+  hardSkills.innerHTML = profileData.skills.hardSkills
     .map(
       (skill) =>
         `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"/></li>`
