d87401caa1f554985b0c1987152b90cc9e125c23
Thallentos <thallescmarques@gmail.com>
2025-05-14 17:16:59 -0300

    Atualiza scripts JS e perfil de dados

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
   const url =
-    "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json";
+    "https://raw.githubusercontent.com/thallentos/Portifolio/refs/heads/main/data/profile.json";
   const fetching = await fetch(url);
   return await fetching.json();
 }
